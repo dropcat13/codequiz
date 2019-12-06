6063e0217be317b07c241143187be59384b3c09e
Cat Kretzschmar <cat.ktz@gmail.com>
2019-12-05 21:13:19 -0500

    timer displays correctly

--- script.js
+++ script.js
@@ -3,22 +3,33 @@ var openQuiz = document.querySelector("#openQuiz");
 var corner = document.querySelector("#corner");
 var bodyEl = document.createElement("ul");
 var timerEl = document.querySelector("#time");
+var quiztitle = document.querySelector("#quiztitle");
 var secondsDisplay;
 var totalSeconds = 0;
 var timer;
 var userScore = 0;
 var userName;
-
+var i = 0;
 
 startQuiz.addEventListener("click", start);
 
-function start(){
-    removeStartinginfo ();
-    startTimer();
-    startQuestions();
+// gets a click from one of the answer buttons and displays the next question. Event bubbling? 
+// document.addEventListener("click", ".ansBtn", nextQuestion);
+document.addEventListener('click',function(event){
+  event.stopPropagation();
+  // if(event.target.class == ('ansBtn') ){
+    quizScore()
+    nextQuestion()   
+  //  }
+});
+
+function start() {
+  removeStartinginfo();
+  startTimer();
+  startQuestions();
 }
 
-function removeStartinginfo(){
+function removeStartinginfo() {
   var quiz = document.getElementById("startquiz");
   quiz.remove();
   var quizheading = document.getElementById("codequiz");
@@ -26,82 +37,67 @@ function removeStartinginfo(){
 }
 
 function startTimer() {
- totalSeconds = questions.length * 15; 
- console.log (questions.length);
-  var timeInterval = setInterval(function() {
+  totalSeconds = questions.length * 15;
+  console.log(questions.length);
+  var timeInterval = setInterval(function () {
     totalSeconds--;
-    timerEl.textContent = totalSeconds + " seconds remaining"; 
+    timerEl.textContent = `${totalSeconds} seconds remaining`;
 
-    console.log ("im doing something")
-        if (totalSeconds < 10) {
-          timerEl = "0" + totalSeconds;
-          } else {
-            timerEl = totalSeconds;
-          }        
-        if (totalSeconds === 0) {
-          alert("Time's up!")
-          clearInterval(timeInterval);
-        }       
-  }, 1000); 
-} 
-
-
-function startQuestions(){
-  for (i=0; i<questions.length; i++){
-    console.log (i);
-    question = questions[i].title;
-    quiztitle.append(question);
-    document.getElementById("quiztitle").style.textAlign = "center";
-    for (j=0; j<questions.choices.length; j++){
-    var btn = document.createElement("BUTTON");     
-    btn.innerHTML = questions[i].choices[j];                     
-    document.body.appendChild(btn); 
+    console.log("im counting down seconds")
+    if (totalSeconds < 10) {
+      timerEl.textContent = "0" + totalSeconds + " seconds remaining";
+    } else {
+      timerEl.textContent = totalSeconds + " seconds remaining";
     }
-  }
-//   var question = document.createElement("h2");
-//   question = questions[0].title;
-//   quiztitle.append(question);
-//   document.getElementById("quiztitle").style.textAlign = "center";
-//   var optionOne = document.createElement("button");
-//   optionOne = questions[0].choices[0];
-//   buttonOne.append(optionOne);
-//   document.getElementById("quiz").style.textAlign = "center";
-//   var optionTwo = document.createElement("button");
-//   optionTwo = questions[0].choices[1];
-//   buttonTwo.append(optionTwo);
-//   document.getElementById("answers").style.textAlign = "center";
-//   var optionThree = document.createElement("button");
-//   optionThree = questions[0].choices[2];
-//   buttonThree.append(optionThree);
-//   document.getElementById("answers").style.textAlign = "center";
-//   var optionFour = document.createElement("button");
-//   optionFour = questions[0].choices[3];
-//   buttonFour.append(optionFour);
-//   document.getElementById("answers").style.textAlign = "center";
+    if (totalSeconds === 0) {
+      alert("Time's up!")
+      clearInterval(timeInterval);
+    }
+  }, 1000);
 }
 
-// quiz function 
-function quizScore(){
-  // .addEventListener("click", start);
-  if (choices === answer){
-    userScore++
+function nextQuestion() {
+  i++;
+  startQuestions()
+}
+
+function startQuestions() {
+  console.log(i);
+  if (i<questions.length) {
+    document.getElementById("quiztitle").style.textAlign = "center";
+    quiztitle.append(questions[i].title);
+    for (j = 0; j < questions[i].choices.length; j++) {
+      var btn = document.createElement("button");
+      btn.classList.add("ansBtn");
+      btn.innerHTML = questions[i].choices[j];
+      quiztitle.appendChild(btn);
+    }
+  } 
+}
+
+function quizScore() {
+  if ( event.currentTarget === questions[i].answer) {
+    userScore++;
   } else {
-    timerEl - 10
+    totalSeconds - 10;
   }
 }
-// registers click and if user guesses correctly increments userScore by 1
-
-// if user guesses incorrectly it subtracts 10 seconds from the timer. 
 
 // at the end of the game, there is an option to enter your name and score as a high score
-document.getElementById("quiz").style.textAlign = "center";
-
+function endOfQuiz() {
+  document.getElementById("quiz").style.textAlign = "center";
+  var quizComplete = document.createElement("h2");
+  quizComplete.textContent = "You've completed the quiz. Your score is " + userScore + ". Enter your name into highscores!";
+  quiz.appendChild(quizComplete);
+  var formEl = document.createElement("form").placeholder = "Type name here..";
+  quiz.appendChild(formEl);
+}
 
 // function saves userscore to local storage
 // creates a do you want to save your highscore button
 // on that click 
 var highScore = localStorage.getItem("highScore");
-localStorage.setItem ("highScore", highScore);
+localStorage.setItem("highScore", highScore);
 localStorage.setItem("userName", userName);
 
 // when game is played again userscore and name is still stored when they finish the next time.
