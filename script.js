cd68dfb1ec63758824731c041011d6e8197aee71
Cat Kretzschmar <cat.ktz@gmail.com>
2019-12-05 23:50:36 -0500

    restart button added

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 var startQuiz = document.querySelector("#startquiz");
 var openQuiz = document.querySelector("#openQuiz");
-var corner = document.querySelector("#corner");
 var bodyEl = document.createElement("ul");
 var timerEl = document.querySelector("#time");
 var quiztitle = document.querySelector("#quiztitle");
 var answers = document.querySelector('#answers');
+var highscores = document.querySelector('#highscores');
 var secondsDisplay;
 var totalSeconds = 0;
 var timer;
 var userScore = 0;
-var userName;
+var userName = "";
 var i = 0;
+var restart;
 
 startQuiz.addEventListener("click", start);
 
@@ -28,6 +29,8 @@ document.addEventListener('click',function(event){
     }  
    }
 });
+
+restart.addEventListener("click", start);
 
 function start() {
   removeStartinginfo();
@@ -55,7 +58,7 @@ function startTimer() {
     } else {
       timerEl.textContent = totalSeconds + " seconds remaining";
     }
-    if (totalSeconds === 0) {
+    if (totalSeconds === 0 || i > 4) {
       alert("Time's up!")
       clearInterval(timeInterval);
     }
@@ -90,22 +93,38 @@ function quizScore() {
   i++;
 }
 
-// at the end of the game, there is an option to enter your name and score as a high score
 function endOfQuiz() {
   // stop Timer
   document.getElementById("quiz").style.textAlign = "center";
   var quizComplete = document.createElement("h2");
   quizComplete.textContent = "You've completed the quiz. Your score is " + userScore + ". Enter your name into highscores!";
   quiz.appendChild(quizComplete);
-  var formEl = document.createElement("form").placeholder = "Type name here..";
-  quiz.appendChild(formEl);
+
+  var endtitle = document.getElementById("quiztitle");
+  endtitle.remove();
+  var end = document.getElementById("answers");
+  end.remove();
+
+  userName = document.createElement("input");
+  userName.innerHTML = 'Your Name';
+  highscores.appendChild(userName);
+
+  var restart = document.createElement("button");
+  restart.innerHTML = "RESTART";
+  highscores.appendChild(restart);
 }
 
 // function saves userscore to local storage
 // creates a do you want to save your highscore button
 // on that click 
-var highScore = localStorage.getItem("highScore");
-localStorage.setItem("highScore", highScore);
+const userInfo = {
+  name: "userName",
+  score: "userScore",
+}
+
+window.localStorage.setItem('userInfo', JSON.stringify(person));
+
+localStorage.setItem("userScore", userScore);
 localStorage.setItem("userName", userName);
 
 // when game is played again userscore and name is still stored when they finish the next time.
